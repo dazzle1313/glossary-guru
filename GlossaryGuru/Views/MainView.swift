73c8a4aa8b42257bedaa29ad7430f9fcdbd30b88
dazzle1313 <work.pavelek@mail.ru>
2024-09-09 11:16:46 +0300

    feat: added logic for constants (colors, icons and etc) & updated ui of main screen

--- GlossaryGuru/Views/MainView.swift
+++ GlossaryGuru/Views/MainView.swift
@@ -14,34 +14,48 @@ struct MainView: View {
     
     var body: some View {
         NavigationView {
-            VStack {
+            VStack(spacing: 20) {
                 HStack {
                     NavigationLink(destination: WordsOfPageView(viewModel: viewModel)) {
-                        Image(systemName: "arrow.left.to.line.compact")
+                        Constants.Icons.book
                             .resizable()
-                            .frame(width: 20, height: 20)
+                            .frame(width: 30, height: 24)
                     }
-                    .padding()
+                    .padding(.leading, 24)
                     Spacer()
                     NavigationLink(destination: AddingWordView(viewModel: viewModel)) {
                         Image(systemName: "plus")
                             .resizable()
                             .frame(width: 20, height: 20)
                     }
-                    .padding()
+                    .padding(.trailing, 24)
+                    .foregroundStyle(Constants.Colors.black)
                 }
-                Text("Слова:")
-                TextField("Введите слово", text: $searchingWord)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                    .padding()
-                    .onChange(of: searchingWord) { newWord in
-                        viewModel.filterWords(searchText: newWord)
-                    }
+                HStack(spacing: 12) {
+                    Constants.Icons.searching
+                        .foregroundStyle(Constants.Colors.black)
+                        .padding(.leading, 12)
+                    TextField("Введите слово", text: $searchingWord)
+                        .textFieldStyle(PlainTextFieldStyle())
+                        .font(Constants.Fonts.mainFont(size: 16))
+                        .padding(.trailing, -12)
+                        .onChange(of: searchingWord) { newWord in
+                            viewModel.filterWords(searchText: newWord)
+                        }
+                    Spacer(minLength: 12)
+                }
+                .background(RoundedRectangle(cornerRadius: 12).fill(Constants.Colors.backgroundGray).frame(height: 36))
+                .padding(.horizontal, 24)
                 List(viewModel.filteredWords) { word in
                     NavigationLink(destination: WordDetailedView(word: word, viewModel: viewModel)) {
                         Text(word.word ?? "")
+                            .font(Constants.Fonts.mainFont(size: 18))
                     }
+                    .padding(.bottom, 8)
+                    .padding(.top, 12)
                 }
+                .listStyle(PlainListStyle())
+                .padding(.leading, 8)
             }
         }
     }
